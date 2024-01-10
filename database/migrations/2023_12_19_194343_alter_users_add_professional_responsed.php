dd91fff3c32ef6cd3051b50eec61a3606f34c278
César Augusto <cesaraugustobispo@gmail.com>
2024-01-09 21:03:28 -0300

    create new column at users table

--- database/migrations/2023_12_19_194343_alter_users_add_professional_responsed.php
+++ database/migrations/2023_12_19_194343_alter_users_add_professional_responsed.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ProfessionalResponsed extends Migration
+class AlterUsersAddProfessionalResponsed extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,9 @@ class ProfessionalResponsed extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('professional_responsed')->nullable();
+        });
     }
 
     /**
@@ -23,6 +25,8 @@ class ProfessionalResponsed extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('professional_responsed');
+        });
     }
 }
