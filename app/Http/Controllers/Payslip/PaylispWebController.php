3aecee2e74d285f0c23129d32a32d9b2819de214
Davi Alvarenga <davialvarenga@hotmail.com.br>
2024-01-11 00:51:38 -0300

    Created the paylisp controller

---
+++ app/Http/Controllers/Payslip/PaylispWebController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaylispWebController extends Controller
+{
+    public function contraCheque(){
+
+        return view('welcome');
+    }
+}
