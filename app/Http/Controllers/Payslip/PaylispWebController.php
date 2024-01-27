f75724cbbdcd5e829afe7cc61b7c7a3262a1515d
Davi Alvarenga <davialvarenga@hotmail.com.br>
2024-01-27 15:25:18 -0300

    add: join teste

--- app/Http/Controllers/Payslip/PaylispWebController.php
+++ app/Http/Controllers/Payslip/PaylispWebController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Payslip;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PaylispWebController extends Controller
 {
     public function index()
     {
+        $users = DB::table('users')->get();
+         
+        foreach ($users as $user) {
+            echo $user->name;
+        }
+       
         return view('paycheck.teste');
     }
 }
