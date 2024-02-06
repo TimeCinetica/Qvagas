cefcb116f43402164cdcad8931f9735653bf2d7e
Caio <caio.silva.16@edu.ufes.br>
2024-02-05 22:18:30 -0300

    alter: table user and paycheck

---
+++ database/migrations/2024_02_06_004501_alter_status_paycheck_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterStatusPaycheckTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('paycheck', function (Blueprint $table) {
+          $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
