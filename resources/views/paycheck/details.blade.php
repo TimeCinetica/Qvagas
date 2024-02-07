ce82ee7d6f01599f17eca934a370384084793f09
Kauã Silva <kaua.jesuscristo78@gmail.com>
2024-02-07 16:03:52 -0300

    composer configurado e collapse feito

--- resources/views/paycheck/details.blade.php
+++ resources/views/paycheck/details.blade.php
@@ -26,40 +26,51 @@
                     </button>
                 </div>
             </form>
-            <table id="list-admins-table" class="table table-striped fast-table">
+            <table id="list-colaborators-table" class="table table-striped fast-table">
                 <thead>
                     <tr>
-                        {{--<th class="text-center ordeble-column" onclick="orderBy()">Nome <i id="order-filter-icon" class="bi bi-caret-down"></i></th>--}}
-                        <th class="text-center">Nome</th>
-                        <th class="text-center">Email</th>
-                        <th class="text-center">Cargo</th>
-                        <th class="text-center">Data de Cadastro</th>
-                        <th class="text-center">Contracheques</th>
+                        <th class="text-start">Email</th>
+                        <th class="text-start">Cargo</th>
+                        <th class="text-start">Data de Cadastro</th>
+                        <th class="text-start">Contracheques</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($users as $user)
-                    <tr>
-                        <div class="d-flex ">
-                        <td class="text-center">
-                            <a href="{{route("paycheck.collaborator", $user->id)}}">{{ $user->name }}</a>
-                        </td>
-                        <td class="text-center">{{$user->email}}</td>
-                        <td class="text-center">{{$user->roleId}}</td>
-                        <td class="text-center">{{$user->created_at}}</td>
-                        <td class="text-center">
-                            @foreach ($user->paychecks as $paycheck)
-                                <div>
-                                    <a href="{{ Storage::url($paycheck->paycheckpdf) }}">Ver Contracheque</a>
-                                </div>
-                            @endforeach
-                            <center><a type="button" class="btn btn-primary btn-dft" onclick="addPaycheck('{{ $user->name }}')">
-                            <i class="bi bi-plus-square"></i>
-                            </a></center>
-                        </td>
+                @foreach ($users as $user)
+                <tr data-toggle="collapse" data-target="#user-{{ $user->id }}" class="accordion-toggle" style="cursor: pointer;">
+                    <td colspan="5" class="text-center">
+                        <strong>{{ $user->name }}</strong>
+                        <i class="bi bi-chevron-down" id="arrow-{{ $user->id }}"></i>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5" style="padding: 0 !important;">
+                        <div class="collapse" id="user-{{ $user->id }}" >
+                            <table class="table">
+                                <tr>
+                                    <td class="text-start">{{$user->email}}</td>
+                                    <td class="text-start">{{$user->roleId}}</td>
+                                    <td class="text-start">{{$user->created_at}}</td>
+                                    <td class="text-start">@foreach ($user->paychecks as $paycheck)
+                                        <a href="{{ Storage::url($paycheck->paycheckpdf) }}">Ver Contracheque</a><br>
+                                    @endforeach
+                                    <center>
+                                        <a type="button" class="btn btn-primary btn-dft" onclick="addPaycheck('{{ $user->name }}')">
+                                            <i class="bi bi-plus-square"></i>
+                                        </a>
+                                    </center></td>
+                                </tr>
+                                    
+                                
+                            </table>
                         </div>
-                    </tr>
-                    @endforeach
+                    </td>
+                </tr>
+                @endforeach
+
+
+
+
                 </tbody>
             </table>
             
@@ -70,6 +81,17 @@
 
 
     </div>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <script>
+    document.querySelectorAll('.accordion-toggle').forEach(function(userRow) {
+        userRow.addEventListener('click', function() {
+            var arrowIcon = this.querySelector('i');
+            arrowIcon.classList.toggle('bi-chevron-down');
+            arrowIcon.classList.toggle('bi-chevron-up');
+        });
+    });
+    </script>
     
 </body>
 </html>
