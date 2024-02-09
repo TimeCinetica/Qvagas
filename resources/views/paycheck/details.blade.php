58e968367b3e5f03a533ff049faf12f844d34594
Fulano de Tal <kaua.jesuscristo78@gmail.com>
2024-02-09 16:59:16 -0300

    ajeitando o design

--- resources/views/paycheck/details.blade.php
+++ resources/views/paycheck/details.blade.php
@@ -27,8 +27,8 @@
     @include('shared.nav')
     <div class="container">
     <div class="col">
-            <div class="row">
-                <div class="header">
+            <div class="row mt-5">
+                <div class="header d-flex gap-5">
                     <h2>Lista de colaboradores</h2>
                     <a type="button" class="btn btn-primary btn-dft" onclick="addCooperator()">
                     Adicionar <i class="bi bi-plus-square"></i>
@@ -43,7 +43,7 @@
                     </button>
                 </div>
             </form>
-            <table id="list-colaborators-table" class="table table-striped fast-table">
+            <table id="list-colaborators-table" class="table table-striped fast-table" style="table-layout: fixed;">
                 <thead>
                     <tr>
                         <th class="text-center">Profissão</th>
@@ -58,14 +58,20 @@
                 @foreach ($users as $user)
                 <tr data-toggle="collapse" data-target="#user-{{ $user->id }}" class="accordion-toggle" style="cursor: pointer;">
                     <td colspan="6" class="text-center">
-                        <strong>{{ $user->name }}</strong>
-                        <i class="bi bi-chevron-down" id="arrow-{{ $user->id }}"></i>
+                        <div class="d-flex align-items-center justify-content-between">
+                            <div></div>
+                            <div><strong>{{ $user->name }}</strong></div>
+                            
+                            <div><i class="bi bi-chevron-down" id="arrow-{{ $user->id }}"></i></div>
+                            
+
+                        </div>
                     </td>
                 </tr>
                 <tr>
                     <td colspan="6" style="padding: 0 !important;">
                         <div class="collapse" id="user-{{ $user->id }}" >
-                            <table class="table">
+                            <table class="table" style="table-layout: fixed;">
                                 <tr>
                                     <td class="text-center">{{$user->job}}</td>
                                     <td class="text-center">{{$user->email}}</td>
@@ -74,11 +80,11 @@
                                     @foreach ($user->paychecks as $paycheck)
                                         <td>                            
                                             <center>
-                                            <a class=" btn btn-primary btn-dft" href="{{ Storage::url($paycheck->paycheckpdf)}}">{{ monthName($paycheck->month_year) }}</a>
+                                            <a class=" btn btn-primary btn-dft" href="{{ Storage::url($paycheck->paycheckpdf)}}" target="_blank">{{ monthName($paycheck->month_year) }}</a>
 
                                             </center>
                                         </td>   
-                                        <td class="text-end">
+                                        <td class="text-center">
                                                 <i class="btn bi-pencil-square btn-sm" onclick="editPaycheck('{{$paycheck->id}}','{{ $user->name }}')"></i>
 
                                                 <i class="btn btn-danger bi-trash btn-smon" onclick="deletePaycheck('{{$paycheck->id}}')"></i>
@@ -88,7 +94,7 @@
                                     @endforeach
                                     <td>
                                         <center>
-                                            <a class="btn btn-primary bi-plus-square btn-dft" onclick="addPaycheck('{{ $user->name }}')"></a>
+                                            <a class="btn btn-primary btn-dft" onclick="addPaycheck('{{ $user->name }}')">Novo<i class="bi bi-plus-square text-white"></i></a>
                                         </center>
                                     </td>
                                 </tr>
